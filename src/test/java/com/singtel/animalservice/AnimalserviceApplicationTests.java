89b3cc4bc250cd966892318a6c4ef1540bfb0232
sugyant <sugyant.chettri@prudential.com.sg>
2021-07-27 21:08:08 +0800

    COMMIT 11: Spring Boot Test Class

---
+++ src/test/java/com/singtel/animalservice/AnimalserviceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.singtel.animalservice;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
